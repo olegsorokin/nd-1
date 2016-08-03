5b50e7a04b2ddb2ecfaf17625c8a012cf3e7d752
Ivan Lemeshev <vanlmshv@gmail.com>
2016-08-04 01:00:30 +0600

    enhance main script

--- task-1/src/main.js
+++ task-1/src/main.js
@@ -1,25 +1,28 @@
-const Pokemon = require('./Pokemon.js');
 const PokemonList = require('./PokemonList.js');
 
-const pokemon1 = new Pokemon('Bulbasaur', 1);
-const pokemon2 = new Pokemon('Ivysaur', 2);
-const pokemon3 = new Pokemon('Venusaur', 3);
-const pokemon4 = new Pokemon('Charmander', 4);
-const pokemon5 = new Pokemon('Charmeleon', 5);
-const pokemon6 = new Pokemon('Charizard', 6);
-const pokemon7 = new Pokemon('Squirtle', 7);
-const pokemon8 = new Pokemon('Wartortle', 8);
-const pokemon9 = new Pokemon('Blastoise', 9);
-const pokemon10 = new Pokemon('Caterpie', 10);
+const pokemons = [
+  ['Bulbasaur', 1],
+  ['Ivysaur', 2],
+  ['Venusaur', 3],
+  ['Charmander', 4],
+  ['Charmeleon', 5],
+  ['Charizard', 6],
+  ['Squirtle', 7],
+  ['Wartortle', 8],
+  ['Blastoise', 9],
+  ['Caterpie', 10],
+  ['Metapod', 11],
+  ['Butterfree', 12],
+  ['Weedle', 13],
+  ['Kakuna', 14],
+];
 
-const lost = new PokemonList(pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6);
-const found = new PokemonList(pokemon7, pokemon8, pokemon9, pokemon10);
+const lost = new PokemonList();
+const found = new PokemonList();
 
-lost.add('Metapod', 11);
-lost.add('Butterfree', 12);
-
-found.add('Weedle', 13);
-found.add('Kakuna', 14);
+pokemons.forEach((pokemon) => {
+  lost.add(pokemon[0], pokemon[1]);
+});
 
 lost.show();
 found.show();
