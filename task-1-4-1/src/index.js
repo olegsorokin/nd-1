75857878a321a97927e07da7f4e6b0dc6729b506
Ivan Lemeshev <vanlmshv@gmail.com>
2016-08-18 01:07:10 +0600

    task 1-4-1

---
+++ task-1-4-1/src/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+const readerStream = fs.createReadStream(`${__dirname}/read.txt`);
+const writerStream = fs.createWriteStream(`${__dirname}/write.txt`);
+const hash = crypto.createHash('md5');
+
+hash.setEncoding('hex');
+
+const onError = error => process.stdout.write(error.stack);
+
+readerStream.on('error', onError);
+writerStream.on('error', onError);
+hash.on('error', onError);
+
+const data = readerStream.pipe(hash);
+
+data.pipe(writerStream);
+data.pipe(process.stdout);
