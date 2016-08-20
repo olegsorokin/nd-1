2e0203b417513aeff321e5d12b4e7fae61e46b2f
Ivan Lemeshev <vanlmshv@gmail.com>
2016-08-20 21:07:23 +0600

    use static method

--- task-1-4-3/src/lib/RandomNumbers.js
+++ task-1-4-3/src/lib/RandomNumbers.js
@@ -2,9 +2,9 @@ const Readable = require('stream').Readable;
 
 class RandomNumbers extends Readable {
   _read() {
-    this.push(String(this.randomNumber()));
+    this.push(String(this.constructor.randomNumber()));
   }
-  randomNumber() {
+  static randomNumber() {
     return Math.floor(Math.random() * 10);
   }
 }
